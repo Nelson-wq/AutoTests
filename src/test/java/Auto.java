c6ef71c0aa546fd2f8ba955f3bb7cb403d9be852
bart <bart@syneforge.com>
2021-02-25 15:10:11 +0200

    auto5.0(Extends)

--- src/test/java/Auto.java
+++ src/test/java/Auto.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-public  class Auto extends Helper{
+public class Auto extends Helper{
 
     @Override
     public void setUp() {
